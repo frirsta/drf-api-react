b97efac252dd695627c7217973591721c980ac49
frirsta <frista-8@hotmail.com>
2023-03-13 20:08:51 +0000

    Add cors_allowed_origin

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -205,4 +205,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 PHONENUMBER_DEFAULT_FORMAT = "INTERNATIONAL"
 
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
