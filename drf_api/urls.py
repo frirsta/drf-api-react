84a412f956394f5af40515912428341d1a724ae3
frirsta <frista-8@hotmail.com>
2023-03-13 21:43:09 +0000

    remove JWT_AUTH_COOKIE = 'my-app-auth'

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import root_route, logout_route
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 urlpatterns = [
     path('', root_route),
