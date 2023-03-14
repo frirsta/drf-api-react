2a8e1816751ef0c5d18c1383583b75050a1406fe
frirsta <frista-8@hotmail.com>
2023-03-14 01:20:56 +0000

    Remove REST_USE_JWT = True

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -46,7 +46,6 @@ JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
-REST_USE_JWT = True
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'}
