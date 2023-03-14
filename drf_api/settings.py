790d5cda46e4035a6869d13d1c8c714dd60b23ae
frirsta <frista-8@hotmail.com>
2023-03-14 12:54:04 +0000

    Remove REST_USE_JWT = True

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -42,7 +42,6 @@ if 'DEV' not in os.environ:
         'rest_framework.renderers.JSONRenderer',
     ]
 
-REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
