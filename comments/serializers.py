ca48a293bd1eec1dbf6c77eb6684ba9eb6e2b35b
frirsta <frista-8@hotmail.com>
2023-03-26 12:49:39 +0000

    Fix type error for CommentSerializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -6,9 +6,9 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    accounts_id = serializers.ReadOnlyField(source='owner.accounts.id')
+    accounts_id = serializers.ReadOnlyField(source='owner.account.id')
     profile_image = serializers.ReadOnlyField(
-        source='owner.accounts.profile_image.url')
+        source='owner.account.profile_image.url')
     created_date = serializers.SerializerMethodField()
     updated_date = serializers.SerializerMethodField()
 
