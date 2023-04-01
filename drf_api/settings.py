d51e24635649c1d52ef6c5b75ec4cd176d8cd25f
frirsta <frista-8@hotmail.com>
2023-04-01 16:21:11 +0000

    Add DATABASE_URL AND SECRET_KEY

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -160,7 +160,7 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-
+    print('connected')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
