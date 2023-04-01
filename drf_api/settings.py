3b95b4512c26ac174a256e0acb282b9cfd1860b4
frirsta <frista-8@hotmail.com>
2023-04-01 16:44:38 +0000

    Add new CSRF_TRUSTED_ORIGINS

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -206,4 +206,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 PHONENUMBER_DEFAULT_FORMAT = "INTERNATIONAL"
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://3000-frirsta-reactfrontend-3oct19f9rjf.ws-eu93.gitpod.io']
+    'https://3000-frirsta-reactfrontend-2bv6gcrqjd6.ws-eu93.gitpod.io/']
