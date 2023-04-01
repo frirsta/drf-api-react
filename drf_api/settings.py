b803a1cb0c13c07018be90ffc760d961afa25500
frirsta <frista-8@hotmail.com>
2023-04-01 15:25:27 +0000

    Remove DEFAULT_PERMISSION_CLASSES

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -32,9 +32,6 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
-    ),
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
