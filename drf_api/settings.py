38b2326da30cd4adb4cd0a41b3acff040f1c3361
Frirsta Ali karem <frirstaalikarem@MacBook-Air-som-tillhor-Frirsta.local>
2023-04-10 14:26:45 +0200

    Remove print('connected')

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -161,7 +161,7 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-print('connected')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
